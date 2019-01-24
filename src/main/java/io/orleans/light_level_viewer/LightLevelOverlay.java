049fe167e845e077e17c8b480152276376dffe03
Louis Orleans <louis@orleans.io>
2019-01-23 17:01:13 -0800

    ♻️ modularizing overlay dependencies

--- src/main/java/io/orleans/light_level_viewer/LightLevelOverlay.java
+++ src/main/java/io/orleans/light_level_viewer/LightLevelOverlay.java
@@ -1,24 +1,26 @@
 package io.orleans.light_level_viewer;
 
-import java.awt.Color;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.EnumLightType;
+import net.minecraft.world.World;
 
 class LightLevelOverlay extends Gui {
-  private Minecraft mc;
   private static final Logger LOGGER = LogManager.getLogger();
 
-  LightLevelOverlay(Minecraft mc) {
-    this.mc = mc;
+  void drawOverlay(FontRenderer fontRenderer, World world, BlockPos pos) {
+    drawString(fontRenderer,
+        String.format("Light (min): %s", getLightLevelBlock(world, pos)), 3, 3,
+        0x88FFFFFF);
   }
 
-  void drawOverlay() {
-    int light_level = mc.world.getLightFor(EnumLightType.BLOCK, mc.player.getPosition())
-        - mc.world.getSkylightSubtracted();
+  int getLightLevelBlock(World world, BlockPos pos) {
+    int light_level = world.getLightFor(EnumLightType.BLOCK, pos)
+        - world.getSkylightSubtracted();
 
-    drawString(mc.fontRenderer, String.format("Light (min): %s", light_level), 3, 3, 0x88FFFFFF);
+    return light_level;
   }
 }
--- src/main/java/io/orleans/light_level_viewer/RiftHook.java
+++ src/main/java/io/orleans/light_level_viewer/RiftHook.java
@@ -3,24 +3,27 @@ package io.orleans.light_level_viewer;
 import net.minecraft.client.Minecraft;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import org.dimdev.rift.listener.client.OverlayRenderer;
 
 /**
- * The example mod listener, as defined in the riftmod.json file
+ * Listeners for Rift mod loader
  *
  * @author Louis Orleans
  */
-public class RiftHook implements OverlayRenderer/* , GameGuiAdder */ {
+public class RiftHook implements OverlayRenderer {
   private static final Logger LOGGER = LogManager.getLogger();
-  private LightLevelOverlay light_level_overlay = new LightLevelOverlay(Minecraft.getInstance());
+  private Minecraft mc = Minecraft.getInstance();
+  private LightLevelOverlay light_level_overlay;
 
   public RiftHook() {
     LOGGER.info("[LLV] Initializing Light Level Viewer");
+
+    light_level_overlay = new LightLevelOverlay();
   }
 
   @Override
   public void renderOverlay() {
-    light_level_overlay.drawOverlay();
+    light_level_overlay.drawOverlay(mc.fontRenderer, mc.world,
+        mc.player.getPosition());
   }
 }
